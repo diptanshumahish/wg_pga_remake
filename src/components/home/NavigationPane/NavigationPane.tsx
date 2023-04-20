13151fd0199bc5aab9b60d888df23ae46bef3d21
diptanshumahish <diptanshumahish2016@gmail.com>
2023-04-20 23:30:00 +0530

    version

--- src/components/home/NavigationPane/NavigationPane.tsx
+++ src/components/home/NavigationPane/NavigationPane.tsx
@@ -110,7 +110,7 @@ export default function NavigationPane() {
           </div>
         </div>
       </div>
-      <span className="text-xs text-white">version 2.0.1 beta</span>
+      <span className="text-xs text-white">version 2.0.2 beta</span>
     </div>
   );
 }
