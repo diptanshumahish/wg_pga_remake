1a1d014ff5c15279932576c90955cff7361f1a50
diptanshumahish <diptanshumahish2016@gmail.com>
2023-04-25 17:46:12 +0530

    adeded

--- src/functions/Admin/getTableComuns.ts
+++ src/functions/Admin/getTableComuns.ts
@@ -2,7 +2,7 @@ const columndata = [
   [
     {
         name: 'Submitted by',
-        selector: row => row.submittedBy,
+        selector: (row: { submittedBy: any; }) => row.submittedBy,
         style: {
             backgroundColor: 'rgba(63, 195, 128, 0.9)',
             color: 'white',
@@ -11,64 +11,63 @@ const columndata = [
     },
     {
         name: 'Name',
-        selector: row => row.Name,
+        selector: (row: { Name: any; }) => row.Name,
 
     },
     {
         name: 'Rate',
-        selector: row => row.Rate,
+        selector: (row: { Rate: any; }) => row.Rate,
 
     },
     {
         name: 'Date',
-        selector: row => row.Date,
+        selector: (row: { Date: any; }) => row.Date,
 
     },
     {
         name: 'E mail',
-        selector: row => row.Email,
+        selector: (row: { Email: any; }) => row.Email,
         grow: 3
 
     },
     {
         name: 'Organization',
-        selector: row => row.Organization,
+        selector: (row: { Organization: any; }) => row.Organization,
         grow: 2
 
     },
     {
         name: 'Mobile Number',
-        selector: row => row.MobileNumber,
+        selector: (row: { MobileNumber: any; }) => row.MobileNumber,
         grow: 2
 
     },
     {
         name: 'Candidate',
-        selector: row => row.Candidate,
+        selector: (row: { Candidate: any; }) => row.Candidate,
         grow: 2
 
     },
     {
         name: 'End Client',
-        selector: row => row.EndClient,
+        selector: (row: { EndClient: any; }) => row.EndClient,
         grow: 2
 
     },
     {
         name: 'Feedback',
-        selector: row => row.Feedback,
+        selector: (row: { Feedback: any; }) => row.Feedback,
 
     },
     {
         name: 'Submission date',
-        selector: row => row.SubmissionDate.toDate().toDateString(),
+        selector: (row: { SubmissionDate: { toDate: () => { (): any; new(): any; toDateString: { (): any; new(): any; }; }; }; })  => row.SubmissionDate.toDate().toDateString(),
         grow: 2
-
 
     },
     {
         name: 'Submission Time',
-        selector: row => `${row.SubmissionDate.toDate().getHours()}:${row.SubmissionDate.toDate().getMinutes()} hrs`
+        selector: (row: { SubmissionDate: { toDate: () => { (): any; new(): any; getHours: { (): any; new(): any; }; getMinutes: { (): any; new(): any; }; }; }; }) => `${row.SubmissionDate.toDate().getHours()}:${row.SubmissionDate.toDate().getMinutes()} hrs`
 
     },
   ],
